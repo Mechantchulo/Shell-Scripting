45124b5b29209ab1e78d0d9dd4ecfec97cc7d55c
Mechantchulo <chulomechant@gmail.com>
2025-10-27 09:38:33 +0300

    Math function in bash

---
+++ mathfunction.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# math function using outdated expr expression
+
+expr 1 + 1
+expr 5 - 1
+
+#math function using the latest update
+multiplication=$((2*2))
+
+
+echo "Multiplication: $multiplication"
